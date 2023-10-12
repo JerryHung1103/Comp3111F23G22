aee216ca307a408b195aef4d36f4c136f3861fc0
JerryHung1103 <kshung@connect.ust.hk>
2023-10-12 16:31:50 +0800

    Create the Maze Let user input/edit the maze.

---
+++ src/main/java/PlayGame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+
+public class PlayGame {
+    public static void main(String [] args){
+        Maze maze=new Maze();
+        maze.User_input();
+    }
+}

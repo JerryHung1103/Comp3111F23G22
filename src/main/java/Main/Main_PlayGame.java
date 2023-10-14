26db4e7fe3301b0d8d9a037218713380f51651f5
JerryHung1103 <kshung@connect.ust.hk>
2023-10-14 08:04:27 +0800

    Adding state of process

--- src/main/java/Main/Main_PlayGame.java
+++ src/main/java/Main/Main_PlayGame.java
@@ -1,10 +1,36 @@
 package Main;
 import Game_Component.*;//my package
+
+import java.util.Arrays;
+import java.util.List;
+
+import static Algorithm.PathFinder.findAllPaths;
+
 public class Main_PlayGame {
+     static public int GAME_STATE =0;
     public static void main(String [] args){
-        Maze maze=new Maze();
-        maze.User_input();
-        maze.show_maze();
+        if(GAME_STATE==0){
+           new Maze();
+        }
+
+        else if(GAME_STATE==1){
+            System.out.println("Start main");
+            List<List<int[]>> paths = findAllPaths(Maze.map, 0, 0, 1, 1);
+            for (List<int[]> path : paths) {
+                for (int[] point : path) {
+                    System.out.print(Arrays.toString(point) + " ");
+                }
+                System.out.println();
+            }
+        }
+
+//        else if(GAME_STATE==2){
+//
+//        }
+
+
+
+
 
     }
 }

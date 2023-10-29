ac53d19a04495e10e2b7ce2c4c3947397301c6fb
JerryHung1103 <kshung@connect.ust.hk>
2023-10-30 05:50:11 +0800

    fix some null pointer bugs.

--- src/main/java/Algorithm/PathFinder.java
+++ src/main/java/Algorithm/PathFinder.java
@@ -4,76 +4,88 @@ import java.util.List;
 import java.util.PriorityQueue;
 import static java.lang.Math.abs;
 
-    public class PathFinder {
-        public static int city_block_distance(int x1,int y1, int x2, int y2){
-            return abs(x1-x2)+abs(y1-y2);}
-        public static List<int[]> findShortestPath(int[][] map, int startRow, int startCol, int endRow, int endCol) {
-            int rows = map.length;
-            int cols = map[0].length;
-            boolean[][] visited = new boolean[rows][cols];
-            int[][] distance = new int[rows][cols];
-            int[][] previous = new int[rows][cols];
+public class PathFinder {
+    public static int city_block_distance(int x1, int y1, int x2, int y2) {
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
 
-            // Initialize distance array with maximum values
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    distance[i][j] = Integer.MAX_VALUE;
-                }
+    public static List<int[]> findShortestPath(int[][] map, int startRow, int startCol, int endRow, int endCol) {
+        int rows = map.length;
+        int cols = map[0].length;
+        boolean[][] closed = new boolean[rows][cols];
+        int[][] distance = new int[rows][cols];
+        int[][] previous = new int[rows][cols];
+System.out.println("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        // Initialize distance array with maximum values
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                distance[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        // Initialize priority queue for A* algorithm
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> distance[a[0]][a[1]] - distance[b[0]][b[1]]);
+
+        // Add the start position to the queue
+        queue.offer(new int[]{startRow, startCol});
+        distance[startRow][startCol] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+
+            // Check if we reached the destination
+            if (row == endRow && col == endCol) {
+                break;
             }
 
-            // Initialize priority queue for Dijkstra's algorithm
-            PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> distance[a[0]][a[1]] - distance[b[0]][b[1]]);
-            queue.offer(new int[]{startRow, startCol});
-            distance[startRow][startCol] = 0;
+            // Skip if the current position is already closed
+            if (closed[row][col]) {
+                continue;
+            }
 
-            while (!queue.isEmpty()) {
-                int[] current = queue.poll();
-                int row = current[0];
-                int col = current[1];
+            closed[row][col] = true;
 
-                if (visited[row][col]) {
-                    continue;
-                }
+            // Explore neighboring cells
+            int[][] neighbors = {{row + 1, col}, {row - 1, col}, {row, col + 1}, {row, col - 1}};
+            for (int[] neighbor : neighbors) {
+                int newRow = neighbor[0];
+                int newCol = neighbor[1];
 
-                visited[row][col] = true;
+                // Check if the neighbor is within bounds and not blocked
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && map[newRow][newCol] != 1) {
+                    int newDistance = distance[row][col] + 1;
 
-                // Check if we reached the destination
-                if (row == endRow && col == endCol) {
-                    break;
-                }
+                    // Update the distance and previous arrays if a shorter path is found
+                    if (newDistance < distance[newRow][newCol]) {
+                        distance[newRow][newCol] = newDistance;
+                        previous[newRow][newCol] = row * cols + col;
 
-                // Explore neighboring cells
-                int[][] neighbors = {{row + 1, col}, {row - 1, col}, {row, col + 1}, {row, col - 1}};
-                for (int[] neighbor : neighbors) {
-                    int newRow = neighbor[0];
-                    int newCol = neighbor[1];
-
-                    // Check if the neighbor is within bounds and not blocked
-                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && map[newRow][newCol] != 1) {
-                        int newDistance = distance[row][col] + 1;
-
-                        // Update the distance and previous arrays if a shorter path is found
-                        if (newDistance < distance[newRow][newCol]) {
-                            distance[newRow][newCol] = newDistance;
-                            previous[newRow][newCol] = row * cols + col;
-                            queue.offer(new int[]{newRow, newCol});
-                        }
+                        int priority = newDistance + city_block_distance(newRow, newCol, endRow, endCol);
+                        queue.offer(new int[]{newRow, newCol, priority});
                     }
                 }
             }
-
-            // Reconstruct the shortest path from the previous array
-            List<int[]> shortestPath = new ArrayList<>();
-            int currentRow = endRow;
-            int currentCol = endCol;
-            while (currentRow != startRow || currentCol != startCol) {
-                shortestPath.add(0, new int[]{currentRow, currentCol});
-                int prev = previous[currentRow][currentCol];
-                currentRow = prev / cols;
-                currentCol = prev % cols;
-            }
-            shortestPath.add(0, new int[]{startRow, startCol});
-
-            return shortestPath;
         }
+
+        // Check if there is a valid path from start to end
+        if (distance[endRow][endCol] == Integer.MAX_VALUE) {
+            return null; // No path found
+        }
+
+        // Reconstruct the shortest path from the previous array
+        List<int[]> shortestPath = new ArrayList<>();
+        int currentRow = endRow;
+        int currentCol = endCol;
+        while (currentRow != startRow || currentCol != startCol) {
+            shortestPath.add(0, new int[]{currentRow, currentCol});
+            int prev = previous[currentRow][currentCol];
+            currentRow = prev / cols;
+            currentCol = prev % cols;
+        }
+        shortestPath.add(0, new int[]{startRow, startCol});
+
+        return shortestPath;
     }
+}
--- src/main/java/Game_Component/Maze.java
+++ src/main/java/Game_Component/Maze.java
@@ -16,6 +16,8 @@ public class Maze extends JFrame {
     public static int[][] map = new int[ROWS][COLS];
 
     public static JPanel tempPanel = new JPanel();
+    public static int[] entry = new int[2];
+    public static int[] exit = new int[2];
 
     public static Map<JPanel, VertexLocation> mazeMap;
     public Maze() {
@@ -37,6 +39,7 @@ public class Maze extends JFrame {
         buttonPanel.add(confirmButton);
 
         gridPanel.setLayout(new GridLayout(ROWS, COLS));
+        gridPanel.addKeyListener(new KeyboardListener());
 
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
@@ -97,52 +100,82 @@ public class Maze extends JFrame {
         }
         return null;
     }
-    public static void Auto_Generate_Maze() {//function for Task A
+    public static int[][] Auto_Generate_Map() {
+        int[][] map = new int[ROWS][COLS];
         while(true){
-            for (JPanel jPanel : mazeMap.keySet()) {
-                int x = mazeMap.get(jPanel).x;
-                int y = mazeMap.get(jPanel).y;
-                Random random = new Random();
-                double randomNumber = random.nextDouble();
-                if (randomNumber > 0.75)
-                    Maze.mazeMap.put(jPanel, new Barrier(jPanel, x, y));
-                else
-                    Maze.mazeMap.put(jPanel, new ClearVertex(jPanel, x, y));
-            }
-
-            for (; ; ) {
-                Random random = new Random();
-                int rand_entry = random.nextInt(30);
-                JPanel entryP = get_panel(rand_entry, 0);
-                if (mazeMap.get(entryP) instanceof ClearVertex) {
-                    Maze.mazeMap.put(entryP, new Entry(entryP, Maze.mazeMap.get(entryP).x, Maze.mazeMap.get(entryP).y));
-                    break;
+            Random rand = new Random();
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    if (rand.nextDouble() < 0.75) {
+                        map[i][j] = 0;
+                    } else {
+                        map[i][j] = 1;
+                    }
                 }
             }
+            entry=getRandomBoundaryPoint(map);
+            exit=getRandomBoundaryPoint(map);
+            List<int[]> path = findShortestPath(map, entry[0], entry[1], exit[0], exit[1]);
+            if(Maze.Path_Exist(path))break;
+        }
+        return map;
+    }
 
-            for (; ; ) {
-                Random random = new Random();
-                int rand_exit = random.nextInt(30);
-                JPanel exitP = get_panel(rand_exit, 29);
-                if (mazeMap.get(exitP) instanceof ClearVertex) {
-                    Maze.mazeMap.put(exitP, new Exit(exitP, Maze.mazeMap.get(exitP).x, Maze.mazeMap.get(exitP).y));
-                    break;
+    public static int[]getRandomBoundaryPoint(int[][] map) {
+        Random rand = new Random();
+        int[] point = new int[2];
+        int side = rand.nextInt(4);  // Randomly select one of the four sides
+        switch (side) {
+            case 0:  // Top boundary
+                point[0] = 0;
+                point[1] = rand.nextInt(ROWS);
+                break;
+            case 1:  // Right boundary
+                point[0] = rand.nextInt(ROWS);
+                point[1] = 29;
+                break;
+            case 2:  // Bottom boundary
+                point[0] = 29;
+                point[1] = rand.nextInt(ROWS);
+                break;
+            case 3:  // Left boundary
+                point[0] = rand.nextInt(ROWS);
+                point[1] = 0;
+                break;
+        }
+        // Ensure the selected point has a value of 0
+        if (map[point[0]][point[1]] != 0) {
+            return getRandomBoundaryPoint(map);
+        }
+        return point;
+    }
+    public static void Auto_Generate_Maze(){
+        map=Auto_Generate_Map();
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (map[i][j] == 1) {
+                    if (!(mazeMap.get(get_panel(i, j)) instanceof Barrier)) {
+                        mazeMap.put(get_panel(i, j), new Barrier(get_panel(i, j), i, j));
+                        get_panel(i, j).repaint();
+                    }
+                } else if (map[i][j] == 0) {
+
+                        mazeMap.put(get_panel(i, j), new ClearVertex(get_panel(i, j), i, j));
+                        get_panel(i, j).repaint();
+
                 }
-
             }
-            int startRow = Maze.mazeMap.get(Maze.Get_Entry()).x;
-            int startCol = Maze.mazeMap.get(Maze.Get_Entry()).y;
-            int endRow = Maze.mazeMap.get(Maze.Get_Exit()).x;
-            int endCol = Maze.mazeMap.get(Maze.Get_Exit()).y;
+        }
+        mazeMap.put(get_panel(entry[0], entry[1]), new Entry(get_panel( entry[0], entry[1]), entry[0], entry[1]));
+        mazeMap.put(get_panel(exit[0], exit[1]), new Exit(get_panel( exit[0], exit[1]), exit[0], exit[1]));
 
-            List<int[]> shortestPath = findShortestPath(Maze.map, startRow, startCol, endRow, endCol);
-        if (Path_Exist(shortestPath)) break;
     }
 
 
 
 
-    }
+
+
 
     public static void Save_Map() throws IOException {//save map to csv and update the boolean map
         FileWriter writer = new FileWriter("maze.csv");
@@ -178,11 +211,14 @@ public class Maze extends JFrame {
     }
 
     public static boolean Path_Exist( List<int[]> path){
-        return ! (path.size()<city_block_distance(  path.get(0)[0],
-                                                    path.get(0)[1],
-                                                    path.get(path.size()-1)[0],
-                                                    path.get(path.size()-1)[1])
-        );
+        if (path == null) return false;
+        if(path.size()==1)return false;
+        int last=path.size()-1;
+        int sedlast=path.size()-2;
+        if(city_block_distance( path.get(last)[0],path.get(last)[1], path.get(sedlast)[0],  path.get(sedlast)[1])>1)
+                return false;
+
+        return true;
     }
 
     public static boolean Entry_Exist(){
@@ -212,6 +248,35 @@ public class Maze extends JFrame {
         }
         return null;
     }
+
+
+
+    public static void movePlayerUp() {
+        if (Jerry.playerRow > 0 && !(mazeMap.get(get_panel(Jerry.playerRow - 1, Jerry.playerRow)) instanceof Barrier)) {
+            mazeMap.put(get_panel(Jerry.playerRow, Jerry.playerRow), new ClearVertex(get_panel(Jerry.playerRow, Jerry.playerRow), Jerry.playerRow, Jerry.playerRow));
+            Jerry.playerRow--;
+            mazeMap.put(get_panel(Jerry.playerRow, Jerry.playerRow), new Jerry(get_panel(Jerry.playerRow, Jerry.playerRow), Jerry.playerRow, Jerry.playerRow));
+            //updateChasePosition();
+            gridPanel.repaint();
+        }
+    }
+
+
+    public static void init_Jerry(){
+        JPanel entry=Get_Entry();
+        mazeMap.put(entry, new Jerry(entry, mazeMap.get(entry).x,  mazeMap.get(entry).y));
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
 

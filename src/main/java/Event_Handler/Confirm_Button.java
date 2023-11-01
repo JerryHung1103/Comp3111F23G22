ce963d314e933a262c1f469e19e433285ca9615a
Minato_Hana <poonyui2002@gmail.com>
2023-11-01 15:17:57 +0800

    ...

--- src/main/java/Event_Handler/Confirm_Button.java
+++ src/main/java/Event_Handler/Confirm_Button.java
@@ -2,17 +2,31 @@ package Event_Handler;
 
 import Game_Component.Maze;
 import Main.Main_PlayGame;
+
+import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+
 public class Confirm_Button extends MouseAdapter {
+
+
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (Maze.Entry_Exist() && Maze.Exit_Exist()) {
-            Maze.Save_Map();
-            Main_PlayGame.GAME_STATE++;
-            Main_PlayGame.main(new String[0]);
-        } else {
-            System.out.println("你未input出入口啊7頭");
+        try {
+            if(Maze.Entry_Exist() && Maze.Exit_Exist()){
+                Maze.Save_Map();
+                Main_PlayGame.GAME_STATE ++;
+                Main_PlayGame.main(new String[0]);
+            }
+            else{
+                // System.out.println("你未input出入口啊7頭");
+                JOptionPane.showMessageDialog(null, "你未input出入口啊7頭", "Warning", JOptionPane.WARNING_MESSAGE);
+            }
+
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
-   }
+    }
 }

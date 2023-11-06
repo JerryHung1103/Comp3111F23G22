4c9427ebfccef4e27f67c45bfe755aef714e8d7e
Minato_Hana <poonyui2002@gmail.com>
2023-11-06 15:34:00 +0800

    ...

--- src/main/java/Main/Main_PlayGame.java
+++ src/main/java/Main/Main_PlayGame.java
@@ -29,7 +29,7 @@ public class Main_PlayGame {
                 }
                 System.out.println();
             }
-            System.out.println("B=================================================================================================");
+            System.out.println("B=================================================================================================D");
             Maze.Show_Path(shortestPath);
             GAME_STATE++;
             Main_PlayGame.main(new String[0]);

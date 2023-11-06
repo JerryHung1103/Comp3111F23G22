35efc0e6cfac40532502767eb71e2db67c21d76b
kenken5216 <69104204+kenken5216@users.noreply.github.com>
2023-11-06 19:02:32 +0800

    Implement Tom and Jerry image

--- src/main/java/Game_Component/MovingObject.java
+++ src/main/java/Game_Component/MovingObject.java
@@ -8,7 +8,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 import java.util.List;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
 
 import static Algorithm.PathFinder.findShortestPath;
 import static java.lang.Math.abs;
@@ -18,6 +22,8 @@ public class MovingObject extends JPanel implements KeyListener {
     public int TomY;
     private int JerryX;
     private int JerryY;
+    Image tomImage;
+    Image jerryImage;
 
 
     private int[][] barriers = TransposeArray.T(Maze.map);
@@ -40,16 +46,19 @@ public class MovingObject extends JPanel implements KeyListener {
         addKeyListener(this);
         setFocusable(true);
         setPreferredSize(new Dimension(900, 900));
-    }
 
+        try {
+            tomImage = ImageIO.read(new File("src/main/java/Game_Component/tom.jpg"));
+            jerryImage = ImageIO.read(new File("src/main/java/Game_Component/Jerry.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.BLUE);
-        g.fillRect(TomX * 30, TomY * 30, 30, 30);
-        g.setColor(Color.RED);
-        g.fillRect(JerryX * 30, JerryY * 30, 30, 30);
-        g.setColor(Color.BLACK);
+        g.drawImage(tomImage, TomX * 30, TomY * 30, this);
+        g.drawImage(jerryImage, JerryX * 30, JerryY * 30, this);
 
         // Draw barriers
         for (int i = 0; i < 30; i++) {
